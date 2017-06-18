aac631d22f3642044998e24eee9e02c661abec6f
Walter Monecke <wmonecke@gmail.com>
2017-06-18 15:15:21 +0200

    added new eventBus to handle commmunication

---
+++ src/components/Server/ServerDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p>Server Details are currently not updated</p>
+        <p>Server Number: {{ obj.id }} </p>
+        <p>Server Status: {{ obj.status }}</p>
+        <button v-if="showButton === true" type="button" name="button"> Change Current Server Status</button>
+
+    </div>
+
+</template>
+
+<script>
+  import { eventBus } from '../../main'
+
+  export default {
+    props: {
+      data: Object
+    },
+    data: function() {
+      return {
+        showButton: false,
+        obj: { id: "none", status: "unavailable"}
+      }
+    },
+
+    created: function() {
+      eventBus.$on('onShowDetails', (data) => {
+        this.showButton = true;
+        this.obj = data;
+      });
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Server/Servers.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="(obj, i) in servers">
+
+                Server #{{ obj.id }}
+                <br>
+                Status: {{ obj.status }}
+                <br>
+                <button v-on:click="showDetails(i)">Show Details</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  import { eventBus } from '../../main'
+
+  export default {
+    data: function() {
+      return {
+        servers: [
+          { id: 1, status: 'Normal' },
+          { id: 2, status: 'Pending' },
+          { id: 3, status: 'Initializing' },
+          { id: 4, status: 'Booting up' },
+          { id: 5, status: 'Normal' }
+        ]
+      }
+    },
+
+    methods: {
+      showDetails: function(id) {
+        console.log(id);
+        eventBus.$emit('onShowDetails', this.servers[id]);
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
